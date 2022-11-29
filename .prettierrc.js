a39f4234bdd92f9fb85f8a5dfadd6a7849986098
hyy984750073 <984750073@qq.com>
2022-11-29 16:33:27 +0800

    添加ts

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,8 @@ module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  singleQuote: true, // 双引号转单引号
+  trailingComma: 'none', // 多行中是否在最后一行添加逗号 none 不添加
+  "endOfLine": "auto",
+  // "semi": false,  // 结束是否尾随分号
 };
