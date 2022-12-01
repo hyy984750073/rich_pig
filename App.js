0fd8d3a22a742231491e6c16b6e2276496c92ba6
hyy984750073 <984750073@qq.com>
2022-12-01 18:10:12 +0800

    Adjusting header styles 章节

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-function HomeScreen({ navigation }) {
+function HomeScreen({ navigation, route  }) {
+  React.useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params?.post]);
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
@@ -17,6 +24,15 @@ function HomeScreen({ navigation }) {
             otherParam: 'anything you want here',
           });
         }}
+      />
+      <Button
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost', {name: '写邮件'})}
+      />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
+      <Button
+        title="Update the title"
+        onPress={() => navigation.setOptions({ title: 'My home Updated!' })}
       />
     </View>
   );
@@ -44,6 +60,33 @@ function DetailsScreen({ route, navigation }) {
   );
 }
 
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass and merge params back to home screen
+          navigation.navigate({
+            name: 'Home',
+            params: { post: postText },
+            merge: true,
+          });
+        }}
+      />
+    </>
+  );
+}
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -51,10 +94,20 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         {/* <Stack.Screen name="Home" component={HomeScreen} options={{ title: '首页' }} /> */}
-        <Stack.Screen name="Home">
+        <Stack.Screen name="Home" options={{
+          title: '首页',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
           {(props) => <HomeScreen {...props} extraData={'123'} />}
         </Stack.Screen>
         <Stack.Screen name="Details" component={DetailsScreen} initialParams={{ itemId: 42 }} />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} options={({ route }) => ({ title: route.params.name })} />
       </Stack.Navigator>
     </NavigationContainer>
   );
