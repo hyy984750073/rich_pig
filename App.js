6e65b1680f80df25cb675e383d2e4957ae1bc97e
hyy984750073 <984750073@qq.com>
2022-12-01 16:37:44 +0800

    Hello React Navigation  章节学习

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-} from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HelloWorld from './components/HelloWorld.js';
-import FlexBox from './components/FlexBox.js';
 
-const App: () => Node = () => {
+function HomeScreen() {
   return (
-    <SafeAreaView>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <HelloWorld />
-        <FlexBox />
-      </ScrollView>
-    </SafeAreaView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
   );
-};
+}
 
-export default App;
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        {/* <Stack.Screen name="Home" component={HomeScreen} options={{ title: '首页' }} /> */}
+        <Stack.Screen name="Home">
+          {(props) => <HomeScreen {...props} extraData={'123'} />}
+        </Stack.Screen>
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
