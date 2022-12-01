c4cf9c96e333a29328ed7b4c118a08b15f1b58c8
hyy984750073 <984750073@qq.com>
2022-12-01 16:44:20 +0800

    Navigating to a new screen 章节

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import * as React from 'react';
+import { Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
