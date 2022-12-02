f0929fbfded249c0406ba910bd0e46ac905dfae6
hyy984750073 <984750073@qq.com>
2022-12-02 10:55:28 +0800

    Header buttons  学习

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import * as React from 'react';
-import { Button, Text, View, TextInput } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react'
+import { Button, Text, View, TextInput, TouchableOpacity, Image } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 
 function HomeScreen({ navigation, route  }) {
+  const [count, setCount] = React.useState(0);
+
   React.useEffect(() => {
     if (route.params?.post) {
       // Post updated, do something with `route.params.post`
       // For example, send the post to the server
     }
-  }, [route.params?.post]);
+    navigation.setOptions({
+      headerRight: () => ( // 会覆盖Stack.Screen的headerRight
+        <Button onPress={() => setCount((c) => c + 1)} title="Update count" />
+      ),
+    })
+  }, [navigation, route.params?.post]);
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Text>Count: {count}</Text>
       <Button
         title="Go to Details"
         onPress={() => {
@@ -27,6 +35,7 @@ function HomeScreen({ navigation, route  }) {
       />
       <Button
         title="Create post"
+        color='red'
         onPress={() => navigation.navigate('CreatePost', {name: '写邮件'})}
       />
       <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
@@ -87,22 +96,59 @@ function CreatePostScreen({ navigation, route }) {
   );
 }
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 38, height: 38 }}
+      source={require('./images/duban.png')}
+    />
+  );
+}
+
+// TODO: export default function App(){}  和  export default class App extends Component{}  有什么区别
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        {/* <Stack.Screen name="Home" component={HomeScreen} options={{ title: '首页' }} /> */}
-        <Stack.Screen name="Home" options={{
-          title: '首页',
+      {/* Stack.Navigator 作用于整个导航（包含多个屏幕
+          headerMode：
+            float：iOS头部效果
+            screen：Android头部效果
+            none：不显示头部
+          screenOptions：可以声明一些具体的属性,并作用于整个导航
+      */}
+      <Stack.Navigator
+        initialRouteName="Home"
+        headerMode={'none'}
+        screenOptions={{
           headerStyle: {
-            backgroundColor: '#f4511e',
+            backgroundColor: 'green',
           },
           headerTintColor: '#fff',
           headerTitleStyle: {
+            fontWeight: 'bold'
+          }
+        }}>
+        {/* Stack.Screen 仅仅作用于当前屏幕
+              Options：一些属性配置
+         */}
+        {/* <Stack.Screen name="Home" component={HomeScreen} options={{ title: '首页' }} /> */}
+        <Stack.Screen name="Home" options={{
+          title: '首页',
+          headerTitle: (props) => <LogoTitle {...props} />,
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff', // 标题文字颜色
+          headerTitleStyle: {
             fontWeight: 'bold',
           },
+          headerRight:() => (
+            <TouchableOpacity onPress={() => alert('逗你呢，没有什么可收藏的~')}>
+              <Text>收藏</Text>
+            </TouchableOpacity>
+          )
         }}>
           {(props) => <HomeScreen {...props} extraData={'123'} />}
         </Stack.Screen>
