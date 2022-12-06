3566db80adcc5641e110a17531f248193dedfbdd
hyy984750073 <984750073@qq.com>
2022-12-06 17:36:10 +0800

    原始eslint和prettierrc配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,9 +2,6 @@ module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
-  singleQuote: true, // 双引号转单引号
-  trailingComma: 'none', // 多行中是否在最后一行添加逗号 none 不添加
-  "endOfLine": "auto", // 换行风格
-  "semi": false,  // 结束是否尾随分号
-  tabWidth: 2, // 每次缩进的空格数
+  singleQuote: true,
+  trailingComma: 'all',
 };
