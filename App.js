932da928b9d880d950bcd022024f0a9e1c0b6454
hyy984750073 <984750073@qq.com>
2022-12-07 17:21:21 +0800

    redux加减运算demo；@reduxjs/toolkit目前还没用到

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import MyBottomTabs from './src/views/tabs/MyBottomTabs.js'
 
+import { Provider as StoreProvider } from 'react-redux'
+import store from './src/redux/store.js'
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <MyBottomTabs />
-    </NavigationContainer>
+    <StoreProvider store={store}>
+      <NavigationContainer>
+        <MyBottomTabs />
+      </NavigationContainer>
+    </StoreProvider>
   )
 }
--- src/views/myCenter/index.js
+++ src/views/myCenter/index.js
@@ -1,12 +1,28 @@
-import * as React from 'react'
-import { Text, View } from 'react-native'
+// import * as React from 'react'
+import React, { Component } from 'react'
+import { Text, View, Button } from 'react-native'
 
-function MyCenterScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>个人中心</Text>
-    </View>
-  )
+import { connect } from 'react-redux'
+import { increment, decrement } from '../../redux/actions/Counter'
+const mapStateToProps = state => {
+  return {
+    num: state.Counter.num,
+  }
 }
 
-export default MyCenterScreen
+// function MyCenterScreen() {
+class MyCenterScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>个人中心</Text>
+        <Text>-----------------</Text>
+        <Button title={'减运算'} onPress={() => this.props.decrement(1)} />
+        <Text>{this.props.num}</Text>
+        <Button title={'加运算'} onPress={() => this.props.increment(1)} />
+      </View>
+    )
+  }
+}
+
+export default connect(mapStateToProps, { increment, decrement })(MyCenterScreen)
