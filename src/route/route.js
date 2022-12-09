b8fec01950a0c7ee1fad416df6b707ae8f5e272c
hyy984750073 <984750073@qq.com>
2022-12-09 16:56:33 +0800

    route初步写

--- src/route/route.js
+++ src/route/route.js
@@ -1,46 +1,36 @@
-// import { Text } from 'react-native'
-// import { NavigationContainer } from '@react-navigation/native'
+import { StackRouter } from '@react-navigation/native-stack'
 
-const config = {
-  screens: {
-    Details: 'details/:id',
-    CreatePost: 'user',
-    Settings: 'settings',
+import HomeScreen from '@views/HomeScreen.js'
+import DetailsScreen from '@views/DetailsScreen.js'
+import SettingsScreen from '@views/SettingsScreen.js'
+
+const MyApp = StackRouter(
+  {
+    // This is the RouteConfig:
+    Home: {
+      screen: HomeScreen,
+      path: '',
+    },
+    Details: {
+      screen: DetailsScreen,
+      path: 'details/:id',
+    },
+    Settings: {
+      screen: SettingsScreen,
+      path: 'settings',
+    },
+    // Settings: {
+    //   // This can be handy to lazily require a screen:
+    //   getScreen: () => require('Settings').default,
+    //   // Note: Child navigators cannot be configured using getScreen because
+    //   // the router will not be accessible. Navigators must be configured
+    //   // using `screen: MyNavigator`
+    //   path: 'settings',
+    // },
   },
-}
+  {
+    initialRouteName: 'Home',
+  },
+)
 
-const linking = {
-  prefixes: [
-    /* your linking prefixes */
-    'richpig://',
-    'https://richpig.com',
-    'https://*.richpig.com',
-  ],
-  config,
-}
-
-// const state = {
-//   routes: [
-//     {
-//       name: 'rooms',
-//       state: {
-//         routes: [
-//           {
-//             name: 'chat',
-//             params: { user: 'jane' },
-//           },
-//         ],
-//       },
-//     },
-//   ],
-// }
-
-// function App() {
-//   return (
-//     <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
-//       {/* content */}
-//     </NavigationContainer>
-//   )
-// }
-
-export default linking
+export default MyApp
