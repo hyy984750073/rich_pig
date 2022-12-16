596ede4da576159d201a366fdad9a8fc42ba40ab
hyy984750073 <984750073@qq.com>
2022-12-16 16:33:31 +0800

    fetch请求接口

--- src/views/ReduxSecondDemo.js
+++ src/views/ReduxSecondDemo.js
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import { View, Button, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Button, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native'
 import { useSelector } from 'react-redux'
 import { increment, decrement } from 'redux/actions/counter.js'
 import store from '@store/store.js'
@@ -10,6 +10,16 @@ function ReduxScreen({ route }) {
     return state.counter.num
   })
   // console.log(store)
+
+  const [isLoading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+  React.useEffect(() => {
+    fetch('https://reactnative.dev/movies.json')
+      .then(response => response.json())
+      .then(json => setData(json.movies))
+      .catch(error => console.error(error))
+      .finally(() => setLoading(false))
+  }, [])
   return (
     <View style={styles.centerBox}>
       <Text style={styles.fontSize30}>{title}</Text>
@@ -17,6 +27,19 @@ function ReduxScreen({ route }) {
       <Button title="加加" onPress={() => store.dispatch(increment(1))} />
       <Text style={styles.fontSize30}>状态管理num：{num}</Text>
       <Button title="减减" onPress={() => store.dispatch(decrement(1))} />
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>
+              {item.title}, {item.releaseYear}
+            </Text>
+          )}
+        />
+      )}
     </View>
   )
 }
